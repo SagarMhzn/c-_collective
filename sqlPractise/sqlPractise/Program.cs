7347fb40ed286cabd0d88d7568c191f4f2246194
SagarMhzn <maharzansagar4u@gmail.com>
2022-07-06 12:41:33 +0545

    sql connection

--- sqlPractise/sqlPractise/Program.cs
+++ sqlPractise/sqlPractise/Program.cs
@@ -20,12 +20,31 @@ namespace sqlPractise
         {
             string cs = "Data Source=NCCS-151\\SQLEXPRESS;Initial Catalog=dbnccs1;integrated security=true";
             SqlConnection conn = new SqlConnection(cs);
-            conn.Open();
-            if (conn.State == ConnectionState.Open)
+            
+            try
             {
-                Console.WriteLine("database Connected");
-                
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("database Connected");
+                    //adding new table tbl_student
 
+                    string tbl_query = "Create table tbl_student(id int not null primary key,name varchar(50),gender varchar(50), address varchar(50), faculty varchar(50))";
+                    SqlCommand cmd= new SqlCommand(tbl_query, conn);
+                    int res = cmd.ExecuteNonQuery();
+                    if (res == -1)
+                    {
+                        Console.WriteLine("table created");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("database connection failed");
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
