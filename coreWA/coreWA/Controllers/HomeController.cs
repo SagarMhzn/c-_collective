7fdecbd96470fba23c371fa6cce64e4f2ffc2128
SagarMhzn <maharzansagar4u@gmail.com>
2022-07-20 12:55:59 +0545

    ...

---
+++ coreWA/coreWA/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using coreWA.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
+namespace coreWA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secured()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login(string ReturnUrl)
+        {
+            ViewData["returl"] = ReturnUrl;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password, string ReturnUrl)
+        {
+            if(username=="admin" && password=="admin")
+            {  
+                //claim :details
+                //claim identity : from which medium to authorize(token/cookie)
+                //claim principle : to whom you want to logged it
+                 
+                //creating claim
+
+                List<Claim> claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
+                claims.Add(new Claim(ClaimTypes.Name, username));
+                claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+                //creating claim identity
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                //creating principle
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                HttpContext.SignInAsync(claimsPrincipal);
+                return Redirect(ReturnUrl);
+            }
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ coreWA/coreWA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace coreWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
